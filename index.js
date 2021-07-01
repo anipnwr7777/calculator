817a035aa1745ef493fc7185270e46b14d4773b5
ANIRUDH PANWAR <anirudhpanwar7777@gmail.com>
2021-07-01 19:39:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenvalue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log(buttonText);
+
+        if (buttonText == 'X') {
+            buttonText = '*';
+            screenvalue += buttonText;
+            screen.value = screenvalue;
+        }
+
+        else if (buttonText == 'C') {
+            screenvalue = '';
+            screen.value = screenvalue;
+        }
+
+        else if (buttonText == '=') {
+            screen.value = eval(screenvalue);
+        }
+
+        else if (buttonText == '⌫') {
+            screenvalue = screenvalue.slice(0, -1);
+            screen.value = screenvalue;
+        }
+
+        else {
+            screenvalue += buttonText;
+            screen.value = screenvalue;
+        }
+    })
+}
